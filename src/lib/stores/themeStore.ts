2f922f623ee8e3b42fc1e11bdd99a6ee43ee8460
Brianleft <contactobrianleft@gmail.com>
2025-06-15 23:45:09 -0300

    hotfix: theme controller

--- src/lib/stores/themeStore.ts
+++ src/lib/stores/themeStore.ts
@@ -1,76 +1,35 @@
 import { writable } from 'svelte/store';
 import type { Writable } from 'svelte/store';
 
-export const availableThemes: Writable<
-	(
-		| 'light'
-		| 'dark'
-		| 'cupcake'
-		| 'bumblebee'
-		| 'emerald'
-		| 'corporate'
-		| 'synthwave'
-		| 'retro'
-		| 'cyberpunk'
-		| 'valentine'
-		| 'halloween'
-		| 'garden'
-		| 'forest'
-		| 'aqua'
-		| 'lofi'
-		| 'pastel'
-		| 'fantasy'
-		| 'black'
-		| 'luxury'
-		| 'dracula'
-		| 'cmyk'
-		| 'autumn'
-		| 'business'
-		| 'acid'
-		| 'lemonade'
-		| 'night'
-		| 'coffee'
-		| 'winter'
-		| 'silk'
-		| 'abyss'
-		| 'caramellatte'
-		| 'valentine'
-		| 'dim'
-	)[]
-> = writable([
+export const lightThemes: Writable<string[]> = writable([
 	'light',
-	'dark',
 	'cupcake',
 	'bumblebee',
 	'emerald',
 	'corporate',
-	'synthwave',
-	'retro',
-	'cyberpunk',
-	'valentine',
-	'halloween',
 	'garden',
-	'forest',
-	'aqua',
 	'lofi',
-	'pastel',
-	'fantasy',
+	'lemonade',
+	'valentine',
+	'winter',
+	'retro',
+	'cyberpunk'
+]);
+
+export const darkThemes: Writable<string[]> = writable([
+	'dark',
+	'synthwave',
+
+	'halloween',
+	'forest',
 	'black',
 	'luxury',
 	'dracula',
-	'cmyk',
-	'autumn',
-	'dim',
-	'business',
-	'acid',
-	'lemonade',
 	'night',
 	'coffee',
-	'winter',
-	'silk',
 	'abyss',
-	'caramellatte',
-	'valentine'
+	'dim',
+	'aqua'
 ]);
 
 export const theme = writable('abyss'); // Tema predeterminado
